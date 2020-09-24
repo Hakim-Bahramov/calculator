fa66385b44cacba41cd8c25bab270b6f63a6f82b
Hakimbek <66952570+Hakim-Bahramov@users.noreply.github.com>
2020-09-24 22:37:19 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function myFunction(num) {
+	$("input").val($("input").val() + num);
+}
+function myResult() {
+	$("input").val(eval($("input").val()));
+}
+function remove() {
+	$("input").val("");
+}
+
+
+
